124ce1a78c504a7db3bb598a97478b8edee3c7c2
krisharavsj <krisharsv@gmail.com>
2025-07-28 20:53:59 +0530

    oops concept.py

---
+++ oops concept.py
@@ -0,0 +1,10 @@
+class IOString():
+    def __init__(self):
+        self.str1=""
+    def input(self):
+        self.str1(input("enter a string"))
+    def upper(self):
+        print("upper case of your string is",self.str1.upper())
+str1=IOString()
+str1.input()
+str1.upper()
